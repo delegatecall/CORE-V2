0f6b8c680edd739f205bd9f7e3e178f44ae3fd64
leapm <delegatecall@gmail.com>
2020-11-30 16:44:28 +0800

    initial setup

---
+++ test/SimpleStorage.spec.ts
@@ -0,0 +1,25 @@
+import {expect, use} from 'chai';
+import {Contract} from 'ethers';
+import {deployContract, MockProvider, solidity} from 'ethereum-waffle';
+import SimpleStorage from '../build/SimpleStorage.json';
+
+use(solidity);
+
+describe('Simple Storage', () => {
+  const [wallet, walletTo] = new MockProvider().getWallets();
+  let storage: Contract
+
+  beforeEach(async () => {
+    storage = await deployContract(wallet, SimpleStorage);
+  });
+
+  it('Should get the initial storage value successfully', async () => {
+    expect(await storage.get()).to.equal(0);
+  });
+
+  it('Should write the storage value successfully', async () => {
+    const newValue = 10
+    await storage.set(newValue)
+    expect(await storage.get()).to.equal(newValue);
+  });
+});
